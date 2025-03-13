d9ec0fcb9e5262be1880da3803c33fb547c50af7
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2025-03-13 10:52:52 +0300

    integrate raylib

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
-﻿namespace Voronoi
+﻿
+using Raylib_cs;
+
+namespace Voronoi
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-
+            Raylib.SetConfigFlags(ConfigFlags.AlwaysRunWindow | ConfigFlags.ResizableWindow);
+            Raylib.SetTargetFPS(0);
+            Raylib.InitWindow(800, 600, "Voronoi");
+            while(!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Red);
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
         }
     }
 }
